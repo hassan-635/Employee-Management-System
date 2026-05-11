c41936cdcf27403b14f23dfe7bd87c827b371ebd
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-05-11 20:43:55 +0500

    Update Program.cs to launch MainForm instead of Form1

--- Smart EMS/Program.cs
+++ Smart EMS/Program.cs
@@ -1,3 +1,5 @@
+using SmartEPS.WinForms;
+
 namespace Smart_EMS
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace Smart_EMS
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
